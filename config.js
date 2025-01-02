0f53eb6802ef574ef71726dd0b2699c209f064ba
MagicalStorm <116570450+MagicalStorm@users.noreply.github.com>
2025-01-02 11:41:39 -0300

    Update lavalink

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "lava.catfein.com",
-        port: 4000,
-        password: "catfein",
+        host: "lavav4.prplmoe.me",
+        port: 1118,
+        password: "prplmoe.me",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
